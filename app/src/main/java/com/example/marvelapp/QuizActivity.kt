0eae2b09274579e790192758109e8def975ca226
Aline-Aguiar <aline.aglilly@gmail.com>
2021-03-28 22:15:17 -0300

    Linha recolocada antes d initViews no onCreate()

--- app/src/main/java/com/example/marvelapp/QuizActivity.kt
+++ app/src/main/java/com/example/marvelapp/QuizActivity.kt
@@ -9,6 +9,7 @@ class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        
         initViews()
     }
 
