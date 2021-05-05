04328467a7f3835be27071031ef10c5385841a10
JeanFFaria <73616975+JeanFFaria@users.noreply.github.com>
2021-05-04 21:21:08 -0300

    Ajustes lógica login

--- app/src/main/java/com/example/marvelapp/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/login/LoginActivity.kt
@@ -37,17 +37,10 @@ class LoginActivity : AppCompatActivity() {
     private val passField by lazy { findViewById<TextInputEditText>(R.id.tiet_password1) }
     private val fieldLayoutEmail by lazy { findViewById<TextInputLayout>(R.id.til_email1) }
     private val fieldLayoutPassword by lazy { findViewById<TextInputLayout>(R.id.til_password1) }
-
-
-    //login google usando firebase abaixo.
-    //EmailtV usado somente para teste
-
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     private val emailTv by lazy { findViewById<TextView>(R.id.tv_message4) }
     private var tryLoginFacebook = false
-
-    //crição do viewmodel
 
     private lateinit var viewModel: LoginActivityViewModel
 
@@ -82,14 +75,11 @@ class LoginActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
 
-//        buttonGoogleLogin.setOnClickListener { signInGoogle(it) } //faltou o it
-
         callbackManager = CallbackManager.Factory.create()
     }
 
+
     //login google usando firebase abaixo:
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -105,7 +95,7 @@ class LoginActivity : AppCompatActivity() {
                 // Google Sign In failed, update UI appropriately
                 Log.w("GoogleSign", "Google sign in failed", e)
             } catch (e: Exception) {
-                setUserEmail("Erro ao efetuar login")
+                setUserName("Erro ao efetuar login")
             }
         }
 
@@ -116,6 +106,87 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    //login google usando firebase abaixo:
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("GoogleSign", "signInWithCredential:success")
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(this, "Welcome ${user.displayName}", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("GoogleSign", "signInWithCredential:failure", task.exception)
+                    setUserName("Erro ao efetuar login")
+                }
+            }
+    }
+
+    fun signInGoogle(view: View) {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 200)
+    }
+
+    fun signInFace(view: View) {
+        loginFacebook()
+        tryLoginFacebook = true
+    }
+
+    private fun loginFacebook() {
+        loginManager.logInWithReadPermissions(this, arrayListOf("email", "public_profile"))
+        loginManager.registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("facebook", "facebook:onSuccess:$loginResult")
+                firebaseAuthWithFacebook(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d("facebook", "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d("facebook", "facebook:onError", error)
+            }
+        })
+    }
+
+    private fun firebaseAuthWithFacebook(token: AccessToken) {
+        Log.d("facebook", "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("facebook", "signInWithCredential:success")
+                    val name = firebaseAuth.currentUser?.displayName
+                    setUserName(name)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("facebook", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    setUserName("Usuário desconectado")
+                }
+            }
+    }
+
+    private fun setUserName(name: String?) {
+        Toast.makeText(
+            baseContext, "Welcome, $name",
+            Toast.LENGTH_SHORT
+        ).show()
+
+    }
 
     private fun createAnAccount() {
         buttonCreateAccount.setOnClickListener {
@@ -143,36 +214,6 @@ class LoginActivity : AppCompatActivity() {
         val password = passField?.text.toString()
 
         viewModel.validateEntryFields(email, password)
-    }
-
-    //login google usando firebase abaixo:
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("GoogleSign", "signInWithCredential:success")
-                    val user = firebaseAuth.currentUser
-                    Toast.makeText(this, "Welcome ${user.displayName}", Toast.LENGTH_LONG).show()
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("GoogleSign", "signInWithCredential:failure", task.exception)
-                    setUserEmail("Erro ao efetuar login")
-                }
-            }
-    }
-
-    private fun setUserEmail(email: String) {
-        emailTv.text = email
-    }
-
-    fun signInGoogle(view: View) {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 200)
     }
 
     private fun configureValidationOfFields() {
@@ -206,64 +247,6 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun loginFacebook() {
-        loginManager.logInWithReadPermissions(this, arrayListOf("email", "public_profile"))
-        loginManager.registerCallback(callbackManager, object :
-            FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                Log.d("facebook", "facebook:onSuccess:$loginResult")
-                handleFacebookAccessToken(loginResult.accessToken)
-
-            }
-
-            override fun onCancel() {
-                Log.d("facebook", "facebook:onCancel")
-            }
-
-            override fun onError(error: FacebookException) {
-                Log.d("facebook", "facebook:onError", error)
-            }
-        })
-    }
-
-    private fun handleFacebookAccessToken(token: AccessToken) {
-        Log.d("facebook", "handleFacebookAccessToken:$token")
-
-        val credential = FacebookAuthProvider.getCredential(token.token)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("facebook", "signInWithCredential:success")
-                    val name = firebaseAuth.currentUser?.displayName
-                    setUserName(name)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("facebook", "signInWithCredential:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    setUserName("Usuário desconectado")
-                }
-            }
-    }
-
-    private fun setUserName(name: String?) {
-        Toast.makeText(
-            baseContext, "Welcome, $name",
-            Toast.LENGTH_SHORT
-        ).show()
-
-    }
-
-
-    fun signingFace(view: View) {
-        loginFacebook()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        tryLoginFacebook = true
-    }
 //
 //    fun signout(view: View) {
 //        firebaseAuth.signOut()
